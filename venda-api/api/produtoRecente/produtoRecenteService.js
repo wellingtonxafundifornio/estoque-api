41fe1a3035023554713a991a66848abd63553991
Wellington <wellingtonxafundifornio@gmail.com>
2017-11-02 17:33:53 -0200

    resolvendo conflitos

--- venda-api/api/produtoRecente/produtoRecenteService.js
+++
@@ -1,6 +0,0 @@
-const  ProdutoRecente = require('./produtoRecente')
-
-ProdutoRecente.methods(['get', 'post', 'put', 'delete'])
-ProdutoRecente.updateOptions({new: true, runValidators: true})
-
-module.exports = ProdutoRecente
